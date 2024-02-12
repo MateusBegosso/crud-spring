ba1a132873a78704bde36c940da957e7f19e0663
Mateus Begosso <mateusbegosso@gmail.com>
2024-02-12 19:55:42 -0300

    feat: Spring API: Create Course (HTTP POST)

--- src/main/java/com/mateus/controller/CourseController.java
+++ src/main/java/com/mateus/controller/CourseController.java
@@ -2,8 +2,14 @@ package com.mateus.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mateus.model.Course;
@@ -21,5 +27,12 @@ public class CourseController {
     @GetMapping
     public List<Course> list() {
         return courseRepository.findAll();
+    }
+
+    @SuppressWarnings("null")
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        return courseRepository.save(course);
     }
 }
